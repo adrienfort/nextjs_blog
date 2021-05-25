70bef07798e17e445ad17130e4f7a1b2e3d0c415
Adrien FORT <adrien.fort01@gmail.com>
2021-05-25 16:29:33 +0200

    feat: add step 5

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 import TopBar from './TopBar';
+import Posts from './Posts';
 
 export default function Home() {
     return (
         <div>
             <TopBar/>
+            <Posts/>
         </div>
 
     )
